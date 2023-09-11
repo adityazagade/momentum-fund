7464e34782496b6fcc9fd69ff2ffb3401fae24d5
Aditya Zagade <adityazagade1@gmail.com>
2023-09-12 00:48:19 +0530

    Update nse_client.py

--- clients/nse_client.py
+++ clients/nse_client.py
@@ -12,7 +12,8 @@ class NSEClient:
         data = self.get_history(symbol=symbol, start=start_date, end=end_date)
         return data
 
-    def get_stock_universe(self, index: str):
+    @staticmethod
+    def get_stock_universe(index: str):
         url_template = "https://archives.nseindia.com/content/indices/ind_{index}list.csv"
         url = url_template.format(index=index.lower().replace(' ', ''))
         df = pd.read_csv(url)
