b02d3c7ca465dbeaf39c75468051788fa7c91235
Aditya Zagade <adityazagade1@gmail.com>
2023-09-24 16:40:38 +0530

    Adding readme

--- services/index_service.py
+++ services/index_service.py
@@ -16,7 +16,9 @@ class IndexDataService:
     def get_index_constituents(self, index: str):
         self.logger.info(f'Getting index constituents for index: {index}')
         stock_universe = self.nse_client.get_stock_universe(index)
-        stock_universe.remove('PATANJALI')  # Some issue with this stock
+        # if PATANJALI is present in stock_universe, remove it
+        if 'PATANJALI' in stock_universe:
+            stock_universe.remove('PATANJALI')  # Some issue with this stock
         # stock_universe =  stock_universe[-5:]
         # stock_universe.append('SBIN')
         return stock_universe
