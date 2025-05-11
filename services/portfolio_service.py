c19a1e079eaa792a8202a7db692f05961e74efc0
Aditya Zagade <aditya.zagade1@gmail.com>
2025-05-11 22:41:13 +0530

    fix lint errors

--- services/portfolio_service.py
+++ services/portfolio_service.py
@@ -1,3 +1,7 @@
+"""
+This module provides services for managing and interacting with portfolios.
+"""
+
 import logging
 
 import pandas as pd
@@ -12,19 +16,36 @@ PORTFOLIO_FILE_KEY = 'portfolio.file'
 
 
 class PortfolioService:
+    """
+    This service class is responsible for managing portfolio operations.
+    """
+
     def __init__(self) -> None:
+        """
+        Initializes the PortfolioService with necessary configurations and clients.
+        """
         super().__init__()
         self.config_service = ConfigService.get_instance()
         self.nse_client = NSEClient.get_instance()
         self.logger = logging.getLogger(__name__)
 
     def get_portfolio(self) -> Portfolio:
+        """
+        Fetches the portfolio by loading it from a file.
+        
+        :return: Portfolio object
+        """
         self.logger.info('Fetching portfolio')
         return self.load_from_file()
 
     def load_from_file(self) -> Portfolio:
+        """
+        Loads the portfolio from a file specified in the configuration.
+        
+        :return: Portfolio object
+        """
         self.logger.info('Loading portfolio from file')
         file = self.config_service.get(PORTFOLIO_FILE_KEY)
         # import csv using pandas
-        df = pd.read_csv(file)
-        return Portfolio.from_df(df)
+        data_frame = pd.read_csv(file)
+        return Portfolio.from_df(data_frame)
